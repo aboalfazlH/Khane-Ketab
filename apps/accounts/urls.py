c0b04db9d8ef8ca053ede6a5c2e9baa28271c7a1
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-02-07 15:52:51 +0330

    Update url

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("sign-up/",views.SignUpView.as_view(),name="sign-up"),
+    path("signup/",views.SignUpView.as_view(),name="signup"),
     path("login/",views.LoginView.as_view(),name="login"),
     path("logout/",views.LogoutView.as_view(),name="logout"),
 ]
