904395ff538b8743fa4de09d17136872a27cde2a
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-02-07 15:42:47 +0330

    Add Login

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = "accounts"
 urlpatterns = [
-    path("sign-up/",views.SignUpView.as_view(),name="sign-up")
+    path("sign-up/",views.SignUpView.as_view(),name="sign-up"),
+    path("login/",views.LoginView.as_view(),name="login")
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,8 @@
 from django.views import generic
 from .forms import SignUpForm
 from django.urls import reverse_lazy
-from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 
 class SignUpView(generic.CreateView):
@@ -9,6 +10,16 @@ class SignUpView(generic.CreateView):
     template_name = "accounts/registration/register.html"
     success_url = reverse_lazy("main-page")
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+
+class LoginView(generic.FormView):
+    form_class = AuthenticationForm
+    template_name = "accounts/registration/register.html"
+    success_url = reverse_lazy("main-page")
+    
     def form_valid(self, form):
         response = super().form_valid(form)
         login(self.request, self.object)
