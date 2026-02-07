cca50452b4ca9b6dac47cf644407e844a356466f
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-02-07 15:24:51 +0330

    Update sign up for auto login

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,11 +1,15 @@
 from django.views import generic
 from .forms import SignUpForm
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate,login
+
 
 class SignUpView(generic.CreateView):
     form_class = SignUpForm
     template_name = "accounts/registration/register.html"
     success_url = reverse_lazy("main-page")
 
-    def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
