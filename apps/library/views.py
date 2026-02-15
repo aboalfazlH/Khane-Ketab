fc745b337597f246a54fdcc86ef5fe906e7fd59b
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-02-15 15:18:17 +0330

    Add a filter for books

--- apps/library/views.py
+++ apps/library/views.py
@@ -10,6 +10,10 @@ class BookListView(generic.ListView):
     model = Book
     context_object_name = "books"
     template_name = "library/book-list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = Book.objects.filter(is_verify=True,is_active=True)
+        return context
 
 class BookCreateView(generic.CreateView):
     form_class = BookForm
