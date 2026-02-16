3273651f99521b39f81f194534a24d2120129fc5
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-02-16 15:09:11 +0330

    Add Model for cart

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.library.models import Book,CustomUser
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    @property
+    def total_price(self):
+        books_in_cart = self.buybook_set.all()
+        total = sum(item.book.price for item in books_in_cart)
+        return total
+
+class BuyBook(models.Model):
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
