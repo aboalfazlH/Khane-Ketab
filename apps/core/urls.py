a836fe72b83fb92db37fd8053255051f25c6d09c
seyed aboalfazl hashemi <saboalfazl6162@gmail.com>
2026-02-25 21:17:48 +0330

    Add better templates

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("",views.MainPageView.as_view(),name="main-page"),
-    path("about/",views.MainPageView.as_view(template_name="main-pages/about.html"),name="main-page"),
-    path("contact/",views.MainPageView.as_view(template_name="main-pages/contact.html"),name="main-page"),
+    path("about/",views.MainPageView.as_view(template_name="main-pages/about.html"),name="about"),
+    path("contact/",views.MainPageView.as_view(template_name="main-pages/contact.html"),name="contact"),
 ]
